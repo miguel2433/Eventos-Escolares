0b775f47371620401c434dc5ee5bd80af8aa6e80
miguel2433 <miguelitocap12@gmail.com>
2024-09-05 17:06:49 -0300

    Salir Register sin icono

--- FrontEnd/js/Login.js
+++ FrontEnd/js/Login.js
@@ -2,6 +2,8 @@ const Registrate = document.querySelector('.registrate');
 const Registro = document.querySelector('.Registro');
 const RegistroInner = document.querySelector('.Registro-inner');
 const Login = document.querySelector('.section');
+const Salir = document.querySelector('.salir-register')
+let Estado = false;
 
 
 function ExpansionDeDominio(){
@@ -11,9 +13,32 @@ function ExpansionDeDominio(){
   // Cambiar el border-radius y border del elemento Login
   Login.style.borderTopRightRadius = '0px';
   Login.style.borderBottomRightRadius = '0px';
-  Login.style.borderRight = '0px';
+  Login.style.borderRight = 'none';
+}
+
+function DominioIncompleto()
+{
+  Registro.classList.remove('visible');
+
+  Login.style.borderTopRightRadius = '20px';
+  Login.style.borderBottomRightRadius = '20px';
+  Login.style.borderRight = '2px solid #d7d7d7';
 }
 
 Registrate.addEventListener('click', function() {
-  ExpansionDeDominio();
+  if(Estado === false)
+  {
+    ExpansionDeDominio();
+    Estado = true;
+  }
 });
+
+Salir.addEventListener('click', function()
+{
+    if(Estado === true)
+    {
+        DominioIncompleto();
+        Estado = false;
+    }
+});
+
