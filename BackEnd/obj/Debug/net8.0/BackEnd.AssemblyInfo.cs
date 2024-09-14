53420340255e08a3aa9600a02d41bb4f8187f23e
miguel2433 <miguelitocap12@gmail.com>
2024-09-14 19:19:33 -0300

    cambios perfil

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26bfe2de2d692410bf80ca5e6cc35296e888b23f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa1c9ee3d99d6bdc938d1fa9d680a35d651ecea")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
