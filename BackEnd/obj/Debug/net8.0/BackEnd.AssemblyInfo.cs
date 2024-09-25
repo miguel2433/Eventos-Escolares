c2955287fdcfaae9b19ca714fdba57db1189fce1
miguel2433 <miguelitocap12@gmail.com>
2024-09-25 13:24:04 -0300

    botones

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a22bab9046e0aefd2272b2a374969dc1d73f961")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0dbfb66a98fb03737e65bf290dcd11e30103471")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FrontEnd/js/CrearEvento.js
+++ FrontEnd/js/CrearEvento.js
@@ -1,6 +1,15 @@
 const BotonGuardar = document.querySelector('.guardar-boton');
-const EditarBoton = document.querySelector('.guardar-boton');
+const EditarBoton = document.querySelector('.editar-boton');
 
-function EnfocadoEditar(){
-    BotonGuardar.style.backgroundColor = '';
+function EnfocadoGuardar(){
+    EditarBoton.style.backgroundColor = 'rgb(100, 100, 196)';
+    EditarBoton.style.color = 'white';
+    EditarBoton.style.border = 'none';
 }
+
+
+BotonGuardar.addEventListener('mouseover', function()
+{
+    EnfocadoGuardar();
+});
+
