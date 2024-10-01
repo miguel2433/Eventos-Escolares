773414a84a574b068e10379252207130ec304720
miguel2433 <miguelitocap12@gmail.com>
2024-09-30 23:13:44 -0300

    text danger

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f652117c2779f8f9f92699eb2f20ce43e85e44f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fd9fca20881880ca6173a1cfecaa2c1265a96db")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
