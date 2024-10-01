a70c61231bc18941df0f05e70ada98ef8dd8a207
miguel2433 <miguelitocap12@gmail.com>
2024-10-01 18:24:59 -0300

    front perfil

--- BackEnd/Models/LoginViewModel.cs
+++ BackEnd/Models/LoginViewModel.cs
@@ -2,13 +2,13 @@ namespace BackEnd.Models
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [Required(ErrorMessage = "La Contraseña es requerida.")]
         public required string ContrasenaLogin { get; set; }
 
-        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Required(ErrorMessage = "El Username es requerido")]
         public required string UsernameLogin { get; set; }
         
-        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Required(ErrorMessage = "El Correo es requerido")]
         [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido")]
         public required string CorreoLogin { get; set; }
 
