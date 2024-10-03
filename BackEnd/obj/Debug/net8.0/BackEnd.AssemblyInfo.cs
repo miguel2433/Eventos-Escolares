b41b9b1f0621c43007e6dd3f41340092f9b6d205
miguel2433 <miguelitocap12@gmail.com>
2024-10-03 12:01:59 -0300

    css

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37c9389eb40ae7968a432c871486c30c806215f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70c61231bc18941df0f05e70ada98ef8dd8a207")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FrontEnd/js/CrearEvento.js
+++ FrontEnd/js/CrearEvento.js
@@ -1,15 +1,19 @@
-const BotonGuardar = document.querySelector('.guardar-boton');
-const EditarBoton = document.querySelector('.editar-boton');
+const guardarBoton = document.querySelector('.guardar-boton');
+const editarBoton = document.querySelector('.editar-boton');
 
-function EnfocadoGuardar(){
-    EditarBoton.style.backgroundColor = 'rgb(100, 100, 196)';
-    EditarBoton.style.color = 'white';
-    EditarBoton.style.border = 'none';
-}
-
-
-BotonGuardar.addEventListener('mouseover', function()
-{
-    EnfocadoGuardar();
-});
-
+guardarBoton.addEventListener('mouseover', () => {
+  editarBoton.classList.add('hover-editar');
+  guardarBoton.classList.add('hover-guardar');
+})
+guardarBoton.addEventListener('mouseout', () => {
+  editarBoton.classList.remove('hover-editar');
+  guardarBoton.classList.remove('hover-guardar');
+})
+editarBoton.addEventListener('mouseover', () => {
+  guardarBoton.classList.add('hover-guardar');
+  editarBoton.classList.add('hover-editar');
+})
+editarBoton.addEventListener('mouseout', () => {
+  guardarBoton.classList.remove('hover-guardar');
+  editarBoton.classList.remove('hover-editar');
+})
