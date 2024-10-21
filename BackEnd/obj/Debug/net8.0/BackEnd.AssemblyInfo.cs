010f58aa9514b390bae34a068f0722c204d98bc4
miguel2433 <miguelitocap12@gmail.com>
2024-10-21 14:59:42 -0300

    estiloz eventos

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+910520e941f276f51677eac4689ff28334d5756c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48213e166dea8f2f2783a9c912013295da80a73b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
